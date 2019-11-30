30ae0461b1fb33e69e00f0039566e787aaf46f9c
numvc <feys-00@mail.ru>
2019-11-30 23:15:43 +0300

    Changed

--- bot.py
+++ bot.py
@@ -9,6 +9,13 @@ bot_id = None
 COMMANDS = {"hello", "help", "do"}
 PICTURE_TYPES = {"png", "jpg"}
 
+@slack.RTMClient.run_on(event='member_joined_channel')
+def on_adiing_user(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    message_handler = MessageHandler(data['channel'])
+    web_client.chat_postMessage(**message_handler.get_help_message(data['user']))
+
 @slack.RTMClient.run_on(event='message')
 def on_message(**payload):
     data = payload['data']
